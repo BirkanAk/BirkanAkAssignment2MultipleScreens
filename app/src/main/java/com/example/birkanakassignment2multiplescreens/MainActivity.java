416b684e968658a396c75cfd38e47b38b8b506de
BirkanAk <bak1@binghamton.edu>
2021-02-25 02:08:03 +0300

    Singleton Created.

--- app/src/main/java/com/example/birkanakassignment2multiplescreens/MainActivity.java
+++ app/src/main/java/com/example/birkanakassignment2multiplescreens/MainActivity.java
@@ -18,4 +18,5 @@ public class MainActivity extends AppCompatActivity {
 
     public void onChangeViewClick(View view) {
     }
+
 }
---
+++ app/src/main/java/com/example/birkanakassignment2multiplescreens/Score.java
@@ -0,0 +1,16 @@
+package com.example.birkanakassignment2multiplescreens;
+
+public class Score {
+    private static Score SCORE=null;
+    public int point =0;
+
+    public static Score getInstance(){
+        if(Score.SCORE==null){
+            Score.SCORE=new Score();
+            return Score.SCORE;
+        }else{
+            return Score.SCORE;
+        }
+
+    }
+}
