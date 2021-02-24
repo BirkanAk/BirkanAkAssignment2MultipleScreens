449b678e3244764edda46d346fa8b5d21a546769
BirkanAk <bak1@binghamton.edu>
2021-02-25 02:59:45 +0300

    Second View Complete!

--- app/src/main/java/com/example/birkanakassignment2multiplescreens/Activity2.java
+++ app/src/main/java/com/example/birkanakassignment2multiplescreens/Activity2.java
@@ -3,12 +3,30 @@ package com.example.birkanakassignment2multiplescreens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
+    Button GambleButton;
+    Button ChangeViewButton2;
+    TextView scoretext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        ChangeViewButton2 = findViewById(R.id.ChangeViewButton2);
+        GambleButton = findViewById(R.id.GambleButton);
+        scoretext = findViewById(R.id.scoretext);
+        scoretext.setText("Score: "+Score.point);
+    }
+
+    public void onChangeViewClick(View view) {
+    }
+
+    public void GambleClick(View view) {
     }
 }
