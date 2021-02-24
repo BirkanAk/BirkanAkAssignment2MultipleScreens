81a2adfb467eb85d740483d63fba396e6a7ccf44
BirkanAk <bak1@binghamton.edu>
2021-02-24 18:32:52 +0300

    First View completed.

--- app/src/main/java/com/example/birkanakassignment2multiplescreens/MainActivity.java
+++ app/src/main/java/com/example/birkanakassignment2multiplescreens/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.birkanakassignment2multiplescreens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onPoint1Click(View view) {
+    }
+
+    public void onChangeViewClick(View view) {
     }
 }
