558a516ce056007f4f75beef6a99972259f9fa2a
BirkanAk <bak1@binghamton.edu>
2021-02-25 03:19:14 +0300

    All Done! Works Perfectly!

--- app/src/main/java/com/example/birkanakassignment2multiplescreens/Activity2.java
+++ app/src/main/java/com/example/birkanakassignment2multiplescreens/Activity2.java
@@ -2,10 +2,13 @@ package com.example.birkanakassignment2multiplescreens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class Activity2 extends AppCompatActivity {
     Button GambleButton;
@@ -25,8 +28,17 @@ public class Activity2 extends AppCompatActivity {
     }
 
     public void onChangeViewClick(View view) {
+        Intent change_view = new Intent(this,MainActivity.class);
+        startActivity(change_view);
     }
 
     public void GambleClick(View view) {
+        Random rn = new Random();
+        int randomNum =  rn.nextInt(21) - 10;
+        //System.out.println(randomNum);
+        Score.getInstance();
+        Score.point+=randomNum;
+        scoretext.setText("Score: "+Score.point);
+
     }
 }
